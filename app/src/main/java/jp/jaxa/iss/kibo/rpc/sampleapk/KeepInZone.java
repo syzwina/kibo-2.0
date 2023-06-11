580f7895b71d24633d20b733acb68913ea0dec9c
Nor Syazwina Binti Ahmad Fazrul Shahril <nor.ahmadfazrulshahril@student.manchester.ac.uk>
2023-06-11 01:45:41 +0100

    Add code from previous repo, and change some methods to reflect new api

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/KeepInZone.java
@@ -0,0 +1,8 @@
+package jp.jaxa.iss.kibo.rpc.sampleapk;
+
+public class KeepInZone extends Zone {
+
+    public KeepInZone(float x_min, float y_min, float z_min, float x_max, float y_max, float z_max) {
+        super(x_min, y_min, z_min, x_max, y_max, z_max);
+    }
+}
