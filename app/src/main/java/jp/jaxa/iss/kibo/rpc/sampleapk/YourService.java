0b2b37003dcfe355a526db79edfe26c746e47cf7
Nor Syazwina Binti Ahmad Fazrul Shahril <nor.ahmadfazrulshahril@student.manchester.ac.uk>
2023-07-23 00:28:18 +0800

    Refactor Zones into Zone Constants

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -36,17 +36,6 @@ import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 public class YourService extends KiboRpcService {
 
     private final String TAG = this.getClass().getSimpleName();
-    // initializing zones
-    private final KeepOutZone KOZ01 = new KeepOutZone(10.783f, -9.8899f, 4.8385f, 11.071f, -9.6929f, 5.0665f);
-    private final KeepOutZone KOZ02 = new KeepOutZone(10.8652f, -9.0734f, 4.3861f, 10.9628f, -8.7314f, 4.6401f);
-    private final KeepOutZone KOZ03 = new KeepOutZone(10.185f, -8.3826f, 4.1475f, 11.665f, -8.2826f, 4.6725f);
-    private final KeepOutZone KOZ04 = new KeepOutZone(10.7955f, -8.0635f, 5.1055f, 11.3525f, -7.7305f, 5.1305f);
-    private final KeepOutZone KOZ05 = new KeepOutZone(10.563f, -7.1449f, 4.6544f, 10.709f, -6.8099f, 4.8164f);
-    List<KeepOutZone> ZONES = Arrays.asList(KOZ01,KOZ02,KOZ03,KOZ04,KOZ05);
-    private KeepOutZone currentKOZ = new KeepOutZone(0f,0f,0f,0f,0f,0f); // not really needed but put it here anyway
-
-    private final KeepInZone KIZ01 = new KeepInZone(10.3f, -10.2f, 4.32f, 11.55f, -6.0f, 5.57f);
-    private final KeepInZone KIZ02 = new KeepInZone(9.5f, -10.5f, 4.02f, 10.5f, -9.6f, 4.8f);
 
     private List<Integer> current_target;
 
